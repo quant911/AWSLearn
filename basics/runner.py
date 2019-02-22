5b5ac8e8f8c0759d63483df77a5e8be5b52cd518
f.n <feng.ning@iaqs.org>
2019-02-22 16:57:10 -0500

    first commit

---
+++ basics/runner.py
@@ -0,0 +1,4 @@
+#the code to be deployed to github and picked up by AWS System Manager to run on EC2
+#https://docs.aws.amazon.com/systems-manager/latest/userguide/integration-remote-scripts.html#integration-github-python
+
+print("hello, AWS EC2!")
